8b0ea135d51d8d634e0052252524abcfc72804cc
Simone Paoletti <simone.paoletti@gucci.com>
2022-10-17 10:03:33 +0200

    Added logo

--- src/App.js
+++ src/App.js
@@ -30,6 +30,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className='logo'>
+          <img src="./whatsaxe.png" /><div>WhatsAxe</div>
+        </div>
         {user && <SignOut auth={auth} />}
       </header>
       <section>
