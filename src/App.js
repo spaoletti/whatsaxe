b67606edfa94173f17ab10759c7359d82a322667
Simone Paoletti <simone.paoletti@gucci.com>
2022-10-30 19:43:38 +0100

    Some more menu styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import React from 'react';
+import React, { useState } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
