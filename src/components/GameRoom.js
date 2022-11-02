9676420b43a7397bbb222dec73c6d3979a7177f8
Simone Paoletti <simone.paoletti@gucci.com>
2022-11-02 22:05:46 +0100

    Refactor

--- src/components/GameRoom.js
+++ src/components/GameRoom.js
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 import { useCollectionData } from "react-firebase-hooks/firestore";
-import { getLastAction, isDM, isFromTheDM, parseCommand } from "../utils";
+import { buildMessage, getLastAction, isDM, isFromTheDM, isPlayer } from "../utils";
 import ChatMessage from "./ChatMessage";
 
 export default function GameRoom(props) {
@@ -8,52 +8,37 @@ export default function GameRoom(props) {
   const messagesRef = props.firestore.collection("messages");
   const query = messagesRef.orderBy("createdAt").limit(100);
   const [messages] = useCollectionData(query);  
-  const [formValue, setFormValue] = useState("");
+  const [inputText, setInputText] = useState("");
   const lastAction = getLastAction(messages);
 
-  const sendMessage = async (messageType) => {
-    const msgText = formValue.trim();
-    if (msgText === "") {
+  const deleteChats = (messagesRef) => {
+    const query = messagesRef.where('type','==',"chat");
+    query.get().then((result) => result.forEach((doc) => doc.ref.delete()));  
+  }
+
+  const sendMessage = async (type) => {
+    const text = inputText.trim();
+    if (text === "") {
       return;
     }
-    setFormValue("");
-    const { uid, photoURL } = props.user;
-    if (messageType === "action") {
-      if (!isDM(props.user) && !isFromTheDM(lastAction)) {
-        return;
-      }
-
-      if (isDM(props.user) && msgText.charAt(0) === "/") {
-        const command = parseCommand(msgText);
-        if (command.error === true) {
-          messagesRef.add({
-            text: `!!! Unknown command: ${command.name} !!!`,
-            createdAt: props.firebase.firestore.FieldValue.serverTimestamp(),
-            uid,
-            photoURL: "https://cdn-icons-png.flaticon.com/512/5219/5219070.png",
-            type: "chat",
-            private: true
-          }).then(() => {
-            bottom.current.scrollIntoView();
-          });
-          return;    
-        } else {
-          // sendCommand
-        }
-      }
-
-      const query = messagesRef.where('type','==',"chat");
-      query.get().then((result) => result.forEach((doc) => doc.ref.delete()));
-    } 
-
+    if (type === "action" && isPlayer(props.user) && !isFromTheDM(lastAction)) {
+      return;
+    }
+    setInputText("");
+    const message = buildMessage(
+      text, 
+      type, 
+      props.user 
+    );
+    if (message.type === "action") {
+      deleteChats(messagesRef);
+    }
     messagesRef.add({
-      text: msgText,
-      createdAt: props.firebase.firestore.FieldValue.serverTimestamp(),
-      uid,
-      photoURL,
-      type: messageType
-    }).then(() => {
-      bottom.current.scrollIntoView();
+      ...message,
+      uid: props.user.uid,
+      createdAt: props.firebase.firestore.FieldValue.serverTimestamp()
+    }).then(() => { 
+      bottom.current.scrollIntoView(); 
     });
   }
 
@@ -68,11 +53,11 @@ export default function GameRoom(props) {
       <form>
         <input 
           data-testid="text" 
-          value={formValue} 
-          onChange={(e) => setFormValue(e.target.value)} 
+          value={inputText} 
+          onChange={(e) => setInputText(e.target.value)} 
         />
         <button 
-          disabled={formValue.length === 0}
+          disabled={inputText.length === 0 || inputText.charAt(0) === "/"}
           onClick={() => sendMessage("chat")} 
           data-testid="send" 
           type="button"
@@ -80,7 +65,7 @@ export default function GameRoom(props) {
           Chat
         </button>
         <button 
-          disabled={formValue.length === 0 || (!isDM(props.user) && (!messages || messages.length === 0 || !isFromTheDM(lastAction)))} 
+          disabled={inputText.length === 0 || (!isDM(props.user) && (!messages || messages.length === 0 || !isFromTheDM(lastAction)))} 
           onClick={() => sendMessage("action")} 
           data-testid="send-action" 
           type="button"
--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,10 @@ export function isDM(userOrMessage) {
   return userOrMessage.uid === "78OjG96RrtZi5J3vqUChlmIdL503";
 }
 
+export function isPlayer(userOrMessage) {
+  return !isDM(userOrMessage);
+}
+
 export function isFromTheDM(message) {
   return message && isDM(message);
 }
@@ -25,4 +29,23 @@ export function parseCommand(commandString) {
     error: true,
     name: name,
   };
+}
+
+export function buildMessage(text, type, user) {
+  if (type === "action" && isDM(user) && text.charAt(0) === "/") {
+    const command = parseCommand(text);
+    if (command.error) {
+      return {
+        text: `!!! Unknown command: ${command.name} !!!`,
+        photoURL: "https://cdn-icons-png.flaticon.com/512/5219/5219070.png",
+        type: "chat",
+        private: true
+      }
+    } 
+  }   
+  return {
+    text: text,
+    photoURL: user.photoURL,
+    type: type
+  };  
 }
